7321942a66547d88cc4bb25fcb81d176bf318fb6
gboone11 <gavin.boone@ejgallo.com>
2025-09-30 21:04:29 -0700

    add falling sand and sim update loop

--- particle.py
+++ particle.py
@@ -14,4 +14,7 @@ class SandParticle(object):
         return int(r*255), int(g*255), int(b*255)
     
     def update(self, grid, row, col):
-        pass
+        if grid.is_cell_empty(row+1, col):
+            return row + 1, col
+        else:
+            return row, col
--- simulation.py
+++ simulation.py
@@ -13,3 +13,13 @@ class Simulation(object):
 
     def remove_particle(self, row, col):
         self.grid.remove_particle(row, col)
+
+    def update(self):
+        for row in range(self.grid.rows):
+            for col in range(self.grid.columns):
+                particle = self.grid.get_cell(row, col):
+                if particle is not None:
+                    new_pos = particle.update(self.grid, row, col)
+                    if new_pos != (row, col):
+                        self.grid.set_cell(new_pos[0], new_pos[1], particle)
+                        self.grid.remove_particle(row, col)
